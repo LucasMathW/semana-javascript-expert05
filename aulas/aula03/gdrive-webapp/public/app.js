992bc63dd254f24de8ca7909ef9b435c5813ea61
matheus <lucasag0408@outlook.com>
2021-10-12 12:48:54 -0400

    add dragAndDropManager.js

--- aulas/aula03/gdrive-webapp/public/app.js
+++ aulas/aula03/gdrive-webapp/public/app.js
@@ -1,14 +1,17 @@
 import AppController from "./src/appController.js";
 import ConnectinManager from "./src/connectionManager.js";
+import DragAndDropManager from "./src/dragAndDropManager.js";
 import ViewManager from  './src/viewManager.js';
 
-const API_URL = 'https://127.0.0.1:3000'
+// const API_URL = 'https://127.0.0.1:3000'
+const API_URL = 'https://gdrive-webapi-lm.herokuapp.com/'
 
 const appControler = new AppController({
   connectionManager: new ConnectinManager({
     apiUrl: API_URL,
   }),
   viewManager: new ViewManager(),
+  dragAndDropManager : new DragAndDropManager()
 })
 
 try{
