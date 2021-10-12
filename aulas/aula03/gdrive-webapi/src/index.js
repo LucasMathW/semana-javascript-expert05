7c68d490f64581199ea450af31357e11e70b4ac2
matheus <lucasag0408@outlook.com>
2021-10-12 11:22:04 -0400

    add environment variable settings

--- aulas/aula03/gdrive-webapi/src/index.js
+++ aulas/aula03/gdrive-webapi/src/index.js
@@ -1,19 +1,26 @@
 import https from 'https'
+import http from  'http'
 import {Server} from 'socket.io'
 import {logger} from './logger.js'
 import fs from 'fs'
 import Routes from './routes.js'
 const PORT = process.env.PORT || 3000
 
+const isProduction = process.env.NODE_ENV === 'production' 
+const user = process.env.USER ?? 'system_user' 
+
 const localHostSSL = {
   key: fs.readFileSync('./certificates/key.pem'),
   cert: fs.readFileSync('./certificates/cert.pem')
 }
 
+const protocol = isProduction ? http : https
+const ssLconfig = isProduction ? {} : localHostSSL
+
 const routes = new Routes()
 
-const server = https.createServer(
-  localHostSSL,
+const server = protocol.createServer(
+  ssLconfig,
   routes.handler.bind(routes)
 )
 
@@ -27,9 +34,10 @@ const io = new Server(server, {
 routes.setSocketInstance(io)
 io.on("connection", (socket) => logger.info(`someone connected: ${socket.id}`))
 
-const startServer =()=> {
+const startServer =()=> { 
+ const protocol = isProduction ? 'http' : 'https'
  const {port, address} = server.address()
- logger.info(`app running at https://${address}:${port}`)
+ logger.info(`app running at ${protocol}://${address}:${port}`)
 }
 
 server.listen(PORT, startServer)
